8f82a990ce36bc871cfc625550ae2ea4f9c0e98c
Dmytro <dmitry.yurin2020@gmail.com>
2024-02-12 12:13:33 +0200

    install shadcn-ui, install clerk, install shadcn dependencies

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "uploadthing.com",
+      "utfs.io",
+      "img.clerk.com",
+      "subdomain",
+      "files.stripe.com",
+    ],
+  },
+  reactStrictMode: false,
+};
+
+module.exports = nextConfig;
